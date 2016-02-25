8a5b74d4509dbfafb6ff4550fce7764c508e4a15
Jake Trimble <zjatreturns@gmail.cm>
2016-02-25 16:13:14 -0700

    creating erb

---
+++ main.rb
@@ -0,0 +1,25 @@
+require "sinatra"
+
+get '/' do
+  erb :main  
+end
+
+get '/cp/' do
+  erb :cp
+end
+
+get '/ls/' do
+  erb :ls
+end
+
+get '/mkdir/' do
+  erb :mkdir
+end
+
+get '/touch/' do
+  erb :touch
+end
+
+get '/mv/' do
+  erb :mv
+end
